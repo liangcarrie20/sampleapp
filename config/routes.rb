ca20541b95c194e0af4991f1abcc6b08254f325c
Carrie Liang <growify.carrie@gmail.com>
2017-09-19 04:41:59 +0100

    Created CCSF Rails pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'ccsf_rails/index'
 
-  get 'static_pages/help'
+  get 'ccsf_rails/links'
 
+  get 'ccsf_rails/about'
+  get 'ccsf_rails/assignments'
+
+  get  'static_pages/home'
+  get  'static_pages/help'
   root 'application#hello'
 end
